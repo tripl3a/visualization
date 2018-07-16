bf6b696ac9e02eb4403a3f493e8109863906970b
tripl3a <triple_a@mailbox.org>
2018-07-16 15:13:07 +0200

    deforest vs land area plot added

--- ePoster/deforestation.R
+++ ePoster/deforestation.R
@@ -4,6 +4,7 @@ library(tidyr)
 library(countrycode)
 library(treemap)
 library(RColorBrewer)
+library(ggplot2)
 
 setwd(dirname(rstudioapi::getSourceEditorContext()$path))
 
@@ -57,16 +58,11 @@ deforest$Ratio1990 = deforest$Ratio1990*-1 #temp change to plot
 deforest$Change1990 = deforest$Change1990*-1 #temp change to plot
 treemap(as.data.frame(deforest[Year==2015 & Ratio1990>0]),
         index=c("Country"),
-        vSize="Ratio1990",
-        palette="Reds",
-        title="Deforestation Ratio 1990 vs. 2015")
-treemap(as.data.frame(deforest[Year==2015 & Ratio1990>0]),
-        index=c("Country"),
         vSize="Change1990",
         vColor="Ratio1990",type="value", #add legend
         title.legend="Deforestation Rate",
         palette="Reds",mapping=c(0, 0.35, 0.7),#range=c(0,0.8)
-        title="Deforestation since 1990")
+        title="Deforestation 1990-2015")
 deforest$Ratio1990 = deforest$Ratio1990*-1 #temp change to plot
 deforest$Change1990 = deforest$Change1990*-1 #temp change to plot
 
@@ -97,8 +93,28 @@ deforest[Year==2015 & is.na(Change1990)]
 deforest[Year==1992 & is.na(Change1990)]
 
 landarea = fread("./data/world-bank_land-area/API_AG.LND.TOTL.K2_DS2_en_csv_v2_9985640.csv",header=T)
+landarea = landarea %>% filter(`Country Name` %in% c("Germany","Netherlands","Poland","Austria","Denmark","Belgium","Switzerland","Czech Republic","France")) %>% select(c("Country Name", "2015"))
+landarea %>% summarise(val=sum(`2015`))
 
-landarea %>% filter(`Country Name` %in% c("Germany","Netherlands","Poland","Austria","Denmark","Belgium","Switzerland","Czech Republic","France","Luxembourg")) %>% select(c("Country Name", "2015"))
-landarea %>% filter(`Country Name` %in% c("Germany","Netherlands","Poland","Austria","Denmark","Belgium","Switzerland","Czech Republic","France","Luxembourg")) %>% summarise(val=sum(`2015`))
+comp = bind_rows(
+  data.table(Group="Land Area",
+             Name=landarea$`Country Name`,
+             sq.km=landarea$`2015`),
+  data.table(Group="Deforested Area",
+             Name=c("Worldwide"),
+             sq.km=(world$Y2015-world$Y1990)*-10)
+  )
+comp=comp[order(rank(sq.km))]
 
+ggplot(data = comp, aes(x=Group, y=sq.km, group = Group)) + theme_bw() +
+  geom_col(aes(fill = Name), colour="black", size=.3) +
+  #scale_fill_brewer(palette="Set3") +
+  scale_fill_grey(start=1,end=1) +
+  geom_text(aes(label = Name), size=3.5, position = position_stack(vjust = 0.5))+ 
+  theme(legend.position="none", axis.title.x=element_blank(), axis.title.y=element_blank(),
+        axis.text.x=element_text(colour="black", size = 10),
+        axis.text.y=element_text(colour="black", size = 10)) +
+  ggtitle("Deforested vs. Land Area (km²)") +
+  scale_y_continuous(breaks=c(500000,1000000,1500000), labels=c("500.000","1.000.000","1.500.000"))
+  
 
